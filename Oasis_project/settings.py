a14c26c4db056e9e8b851422d49a747049e42ffc
tlsgptj <hyeseoshin@DESKTOP-QO9E1VG>
2024-08-18 12:43:34 +0900

    오류 수정

--- Oasis_project/settings.py
+++ Oasis_project/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'Oasis_project.urls'
@@ -131,7 +132,10 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = "Oasis.User"
 
 AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
